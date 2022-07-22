0c7f26984ee03d694d27a8c4441718c607d75204
Watcharakrit Phantu <watcharakrit@gmail.com>
2022-07-22 19:50:29 +0700

    Added: Precommiter and Styled Component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
 function App() {
   return (
@@ -18,9 +19,16 @@ function App() {
         >
           Learn React
         </a>
+        <Box/>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`
